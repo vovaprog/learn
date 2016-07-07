8523357bd515bca75720dda979eb345e16cc3a8d
vova <vovaprog@yandex.ru>
2016-07-07 18:57:09 +0300

    git commands print_hex

--- tools/print_hex.cpp
+++ tools/print_hex.cpp
@@ -1,31 +1,54 @@
 #include <iostream>
 #include <iomanip>
+#include <bitset>
 
-static void printBytesHex(void *buf, int size)
+static void printHex(void *buf, int size)
 {
-    std::cout << std::hex << std::setfill('0') << std::setw(2);
+    std::cout << std::hex;
     std::cout << std::endl << "[ ";
     for(int i=0;i<size;++i)
     {
-        std::cout << (int)((unsigned char*)buf)[i] << " ";
+        std::cout << std::setfill('0') << std::setw(2) << (int)((unsigned char*)buf)[i] << " ";
     }
     std::cout << "]   (size: " << std::dec << size << ")" << std::endl;     
 }
 
-static void printBytesHex(const char *msg, void *buf, int size)
+static void printHex(const char *msg, void *buf, int size)
 {
     std::cout << msg;
-    printBytesHex(buf, size);
+    printHex(buf, size);
 }
+
+static void printBin(void *buf, int size)
+{
+    std::cout << std::endl << "[ ";
+    
+    for(int i=0;i<size;++i)
+    {
+        std::cout << std::bitset<8>(static_cast<unsigned int>((static_cast<unsigned char*>(buf))[i])) << " ";
+    }
+    
+    std::cout << "]   (size: " << std::dec << size << ")" << std::endl;    
+}
+
+static void printBin(const char *msg, void *buf, int size)
+{
+    std::cout << msg;
+    printBin(buf, size);
+}
+
 
 int main()
 {
     char buf[] = {0x11, 0x22, 0x33};
     int size = 3;
     
-    printBytesHex(buf, size);
-    printBytesHex("input: ", buf, size);
-    printBytesHex("output: ", buf, size);
+    printHex(buf, size);
+    printHex("input: ", buf, size);
+    printHex("output: ", buf, size);
+    printBin(buf, size);
+    printBin("input: ", buf, size);
+    printBin("output: ", buf, size);
     
     return 0;
 }
