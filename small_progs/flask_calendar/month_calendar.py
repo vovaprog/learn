bad923d0c80d504845ebc76b97dbc943e9b12517
Vladimir Silaev <vovaprog@yandex.ru>
2016-09-02 08:34:35 +0300

    calendar

--- small_progs/flask_calendar/month_calendar.py
+++ small_progs/flask_calendar/month_calendar.py
@@ -31,17 +31,42 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
-@app.route('/')
+def get_prev_month(year, month):
+    if month == 1:
+        return year-1, 12
+    else:
+        return year, month - 1
+
+def get_next_month(year, month):
+    if month == 12:
+        return year+1, 1
+    else:
+        return year, month + 1
+
+
+@app.route('/',defaults={'year':0, 'month':0})
+@app.route('/<year>/<month>')
 @requires_auth
-def page():
-    cal = Calendar(0)
+def page(year, month):
+    year = int(year)
+    month = int(month)
+    if year==0 or month==0:
+        now = date.today()
+        year = now.year
+        month = now.month
+
+    cal = Calendar(0)    
+    month_cal = cal.monthdatescalendar(year, month)
+
+    data={'month':month_cal}
     
-    month = cal.monthdatescalendar(2016, 9)
-    
-    for week in month:
-        print week
-    
-    return render_template('page.html', data={'month':month})
+    prev_year, prev_month = get_prev_month(year, month)
+    next_year, next_month = get_next_month(year, month)
+
+    data['prev_month_link'] = str.format('/{0}/{1}', prev_year, prev_month)
+    data['next_month_link'] = str.format('/{0}/{1}', next_year, next_month)
+
+    return render_template('page.html', data = data)
 
 
 if __name__ == "__main__":
