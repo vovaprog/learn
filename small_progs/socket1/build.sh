a6001c79a975e56556f6e310e7bb486303edcd44
vova <vovaprog@yandex.ru>
2016-09-07 14:22:00 +0300

    sockets

--- small_progs/socket1/build.sh
+++ small_progs/socket1/build.sh
@@ -4,3 +4,5 @@ g++ -Wall -O2 -std=c++11 -I../../tools ./client.cpp ../../tools/NetworkUtils.cpp
 
 g++ -Wall -O2 -std=c++11 -I../../tools ./server_read.cpp ../../tools/NetworkUtils.cpp -o ./server_read -pthread
 g++ -Wall -O2 -std=c++11 -I../../tools ./client_write.cpp ../../tools/NetworkUtils.cpp -o ./client_write -pthread
+
+g++ -Wall -O2 -std=c++11 -I../../tools ./server_poll.cpp ../../tools/NetworkUtils.cpp -o ./server_poll -pthread
