b6c5d59575139d9f2e32bc589e8839b528fa99ab
Vladimir Silaev <v.silaev@rts.ru>
2016-10-31 19:07:23 +0300

    learn

--- small_progs/create_pch.sh
+++ small_progs/create_pch.sh
@@ -2,6 +2,7 @@
 
 #ubuntu 14.04:
 g++ -std=c++11 -Winvalid-pch -x c++-header -g -O2 -o ./stdc++.h.gch /usr/include/x86_64-linux-gnu/c++/4.8/bits/stdc++.h
+g++ -std=c++11 -Winvalid-pch -x c++-header -g -O2 -pthread -o ./stdc++.h.gch /usr/include/x86_64-linux-gnu/c++/4.8/bits/stdc++.h
 
 #g++ -std=c++11 -Winvalid-pch -x c++-header -g -O2 -o ./stdc++.h.gch /usr/include/x86_64-linux-gnu/c++/5/bits/stdc++.h #-pthread
 
