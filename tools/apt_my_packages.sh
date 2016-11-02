f67042076e25651d9252a6355c5ed7a35ee5a795
Vladimir Silaev <vovaprog@yandex.ru>
2016-11-02 23:36:32 +0300

    apt packages

--- tools/apt_my_packages.sh
+++ tools/apt_my_packages.sh
@@ -1,4 +1,9 @@
 
 apt-get install git gitk htop cmake mc default-jdk geany synaptic libboost-dev astyle vim vim-pathogen exuberant-ctags \
-                cscope
+                cscope gdb python-flask python-flaskext.wtf freeglut3-dev libtinyxml-dev libtinyxml2-dev \
+                libfreeimage-dev libboost-all-dev libglm-dev qtbase5-dev qt5-default ipython
+
+#for python_numpy
+apt-get install python-numpy python-matplotlib python-scipy python-pandas
+
 
