3147a4ebb24b89f5f79310f82c69e89785eec546
Vladimir Silaev <vovaprog@yandex.ru>
2017-01-30 19:32:21 +0300

    learn

--- tools/apt_my_packages.sh
+++ tools/apt_my_packages.sh
@@ -1,9 +1,8 @@
 
 apt-get install git gitk htop cmake mc default-jdk geany synaptic libboost-dev astyle vim vim-pathogen exuberant-ctags \
                 cscope gdb python-flask python-flaskext.wtf freeglut3-dev libtinyxml-dev libtinyxml2-dev \
-                libfreeimage-dev libboost-all-dev libglm-dev qtbase5-dev qt5-default ipython
-
-#for python_numpy
-apt-get install python-numpy python-matplotlib python-scipy python-pandas
+                libfreeimage-dev libboost-all-dev libglm-dev qtbase5-dev qt5-default ipython \
+                python-numpy python-matplotlib python-scipy python-pandas \
+                libssl-dev libtinyxml2-dev jedit
 
 
---
+++ tools/vim/install_nerd_tree.sh
@@ -0,0 +1,4 @@
+
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/jistr/vim-nerdtree-tabs.git ~/.vim/bundle/vim-nerdtree-tabs
+
