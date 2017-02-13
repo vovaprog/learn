c0c54044ca80c69e1b118b066f851e9de7c98abe
Vladimir Silaev <vovaprog@yandex.ru>
2017-02-13 10:39:52 +0300

    get_startup_path

--- tools/get_startup_path/get_startup_path.cpp
+++ tools/get_startup_path/get_startup_path.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 
 int getExecutableName(char *buf, size_t bufSize)
 {
@@ -23,6 +24,44 @@ int getExecutableName(char *buf, size_t bufSize)
 	return 0;
 }
 
+int getStartupPath(char *buf, size_t bufSize)
+{
+	int ret = getExecutableName(buf, bufSize);
+	
+	if(ret != 0)
+	{
+		return ret;
+	}
+	
+	int length = strlen(buf);
+	
+	int i;
+	for(i = length - 1; i > 0; --i)
+	{
+		if(buf[i] == '/')
+		{
+			break;
+		}
+	}
+	
+	for(; i - 1 >= 0; --i)
+	{
+		if(buf[i - 1] != '/')
+		{
+			break;
+		}
+	}
+
+	if(buf[i - 1] == '/')
+	{
+		return EINVAL;
+	}
+	
+	buf[i] = 0;
+	
+	return 0;
+}
+
 int main()
 {
 	char buf[500];
@@ -30,6 +69,11 @@ int main()
 	if(getExecutableName(buf, sizeof(buf)) == 0)
 	{
 		printf("executable name: [%s]\n", buf);	
+	}
+
+	if(getStartupPath(buf, sizeof(buf)) == 0)
+	{
+		printf("startup path: [%s]\n", buf);	
 	}
 	
 	return 0;
