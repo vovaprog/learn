f8027f9684a0ecfe674d82953c306795e4622842
Vladimir Silaev <vovaprog@yandex.ru>
2017-03-31 09:53:51 +0300

    learn

---
+++ small_progs/static_init.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int init(int x)
+{
+	printf("init function: %d\n", x);
+	return x * x;
+}
+
+
+void testStaticInit(int x)
+{
+	static int myResult = init(x);
+	
+	if (myResult != 0)
+	{
+		return;	
+	}
+	
+	printf("%d ", x);
+}
+
+
+int main()
+{
+	for(int i=0;i<=10;++i)
+	{
+		testStaticInit(i);	
+	}		
+	
+    return 0;
+}
